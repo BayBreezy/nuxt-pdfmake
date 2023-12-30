c960ed4fc4bd254c99c296819239bf8da7a6e9a0
BayBreezy <behon.baker@yahoo.com>
2023-12-30 08:14:39 -0500

    chore(docs): add tailwindcss

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,7 +1,8 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: "@nuxt-themes/docus",
-  modules: ["@nuxt/devtools", "nuxt-pdfmake"],
+  modules: ["@nuxt/devtools", "nuxt-pdfmake", "@nuxtjs/tailwindcss"],
+  tailwindcss: { exposeConfig: true },
 
   css: ["~/assets/main.css"],
   app: {
