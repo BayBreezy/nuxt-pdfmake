b6c87752320971dfe236aa98d475bdf9d402e96a
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-07-16 08:14:42 -0400

    fix(module): update compatibility to include nuxt 4

--- src/module.ts
+++ src/module.ts
@@ -34,7 +34,7 @@ export default defineNuxtModule<ModuleOptions>({
     version,
     configKey: "pdfmake",
     compatibility: {
-      nuxt: "^3.0.0",
+      nuxt: "^3.0.0 || ^4.0.0",
     },
   },
   // Default configuration options of the Nuxt module
