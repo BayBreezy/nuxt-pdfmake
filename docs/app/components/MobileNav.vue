74718bc2181f7d61ddae9f0383249f641427d1eb
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-07-16 08:38:59 -0400

    style: format codebase

--- docs/app/components/MobileNav.vue
+++ docs/app/components/MobileNav.vue
@@ -8,7 +8,7 @@
           <div class="bg-background sticky top-0 mb-3 w-full">
             <div class="flex items-center gap-3.5">
               <div class="bg-foreground/10 flex h-8 w-8 items-center justify-center rounded">
-                <img src="/icon.png" alt="Logo" class="size-9 rounded-lg object-contain" >
+                <img src="/icon.png" alt="Logo" class="size-9 rounded-lg object-contain" />
               </div>
               <p class="text-xl font-bold">UI Doc</p>
               <UiSheetClose class="ml-auto">
