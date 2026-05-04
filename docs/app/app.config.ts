72468b82eb1bf652982bdbfb6f80b99e4cab16a7
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2026-05-04 04:55:01 -0500

    docs: enhance app configuration with GitHub links and navigation options & logo

--- docs/app/app.config.ts
+++ docs/app/app.config.ts
@@ -1,8 +1,46 @@
+const repoBase = "https://github.com/BayBreezy/nuxt-pdfmake";
+
 export default defineAppConfig({
   docd: {
+    github: {
+      repo: repoBase,
+      contentDir: "docs/content",
+    },
     ui: {
+      expandNav: true,
+      extraLinks: [
+        {
+          label: "Star on GitHub",
+          icon: "lucide:star",
+          external: true,
+          href: repoBase,
+        },
+        {
+          icon: "lucide:bug",
+          label: "Report an issue",
+          external: true,
+          href: `${repoBase}/issues/new?template=bug_report.yml`,
+        },
+        {
+          icon: "lucide:lightbulb",
+          label: "Feature request",
+          external: true,
+          href: `${repoBase}/issues/new?template=feature_request.yml`,
+        },
+        {
+          icon: "lucide:coffee",
+          label: "Buy me coffee",
+          external: true,
+          href: "https://buymeacoffee.com/llehXIrI8g",
+        },
+      ],
       header: {
         title: "Nuxt pdfMake",
+        logo: {
+          alt: "Nuxt pdfMake Logo",
+          dark: "/icon.png",
+          light: "/icon.png",
+        },
       },
     },
   },
